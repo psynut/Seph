593091a447173789ed6d7ea2318bba3b6e64598f
Michael Sibrava <48391846+psynut@users.noreply.github.com>
2022-05-21 01:12:45 -0700

    Fixed up section mechanics

--- Assets/SectionLoader.cs
+++ Assets/SectionLoader.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class SectionLoader : MonoBehaviour
 {
-    GameObject section;
+    public GameObject section;
 
     // Start is called before the first frame update
     void Start()
     {
-        section = GetComponentInParent<Transform>().gameObject;   
+        Debug.Log(section.name);
     }
 
     // Update is called once per frame
