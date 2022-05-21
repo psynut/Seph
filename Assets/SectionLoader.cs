5675da2482608e316a29248034a8c2623255de34
Michael Sibrava <48391846+psynut@users.noreply.github.com>
2022-05-21 00:40:47 -0700

    A Full Section Is Developed
    Camera is setup to follow the player.

---
+++ Assets/SectionLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionLoader : MonoBehaviour
+{
+    GameObject section;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        section = GetComponentInParent<Transform>().gameObject;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            section.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            section.SetActive(false);
+        }
+    }
+}
