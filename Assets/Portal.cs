5e3a08b44ac3cfc9ecdd974f16bed1f85459a304
Michael Sibrava <48391846+psynut@users.noreply.github.com>
2022-05-23 12:41:10 -0700

    Portal Mostly Working
    Not sure why I made rigidbody cylindar seperate object in heirarchy. Need to test since portal trigger is not identifying the the cylindar rb as player.

---
+++ Assets/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    private Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = GetComponentInChildren<Destination>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("OnTriggerEnter Called " + other.name);
+        if(other.tag == "Player") {
+            other.attachedRigidbody.transform.Translate(destination.transform.position);
+        }
+    }
+}
